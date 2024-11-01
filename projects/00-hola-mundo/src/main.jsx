50b29c86ecaa65e5f4521593b79cc3770b76c406
Leonel <lsaballos74@gmail.com>
2024-11-01 15:24:02 -0600

    Hago algunos nuevos cambios practicando

--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom/client'
 const root = ReactDOM.createRoot(document.getElementById('root')).render(
 <React.Fragment>
 
-  <button>Hola mundo
-  
-  </button>
+  <button>Hola mundo </button>
 </React.Fragment>
 )
